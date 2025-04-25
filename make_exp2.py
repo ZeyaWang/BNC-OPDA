e01d8efe75383823e88cd0b7e7144d376a237873
ZWStatLab <zeya.wang@uky.edu>
2025-04-24 22:58:21 -0400

    update

--- make_exp2.py
+++ make_exp2.py
@@ -75,11 +75,11 @@ for ds, st in source_target.items():
                                                 outline.append(cmd)
                                             else:
                                                 print('======{} exists======'.format(outcsv))
-nn = 4# 7
+nn = 7
 split_lists = [[] for _ in range(nn)]
 for i, element in enumerate(outline):
     split_lists[i % nn].append(element)
-cuda_list = [1,3,5,7]
+cuda_list = [0,1,2,3,5,6,7]
 for ii in range(nn):
     job = 'UDA_{}'.format(ii)
     jobName=job + '.sh'
--- source_free.py
+++ source_free.py
@@ -225,7 +225,7 @@ global_step = 0
 metrics_epoch = {}
 best_hos = 0.
 if args.dataset != 'visda':
-    threshs = [0.1, 0.2, 0.3, 0.4, 0.45, 0.5, 0.55, 0.6, 0.7, 0.8, 0.9]
+    threshs = [0.4, 0.45, 0.5, 0.55, 0.6]
 else:
     threshs = [0.5]
 
