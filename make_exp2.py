7fffdb1fb5d78b7d5e76bd7b0b92e7ceed3d8f92
ZWStatLab <zeya.wang@uky.edu>
2025-05-08 16:45:56 -0400

    update

--- make_exp2.py
+++ make_exp2.py
@@ -84,7 +84,7 @@ for target_type in target_types:
                                                     cmd += '--classifier \n'
                                                 else:
                                                     cmd += '\n'
-                                                outcsv = 'exp3_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.csv'.format(target_type, domain[ds][src], domain[ds][tar], balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc, cl)
+                                                outcsv = 'exp3_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_64.csv'.format(target_type, domain[ds][src], domain[ds][tar], balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc, cl)
                                                 if not os.path.isfile(outcsv):
                                                     outline.append(cmd)
                                                 else:
