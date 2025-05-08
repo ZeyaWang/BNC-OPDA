66ef3672619f385b2ee7283af9e7035aa77940c7
ZWStatLab <zeya.wang@uky.edu>
2025-05-08 11:50:15 -0400

    update

--- make_exp2.py
+++ make_exp2.py
@@ -89,12 +89,12 @@ for target_type in target_types:
                                                     outline.append(cmd)
                                                 else:
                                                     print('======{} exists======'.format(outcsv))
-nn = 6
+nn = 7
 split_lists = [[] for _ in range(nn)]
 for i, element in enumerate(outline):
     split_lists[i % nn].append(element)
 cuda_list = [1,2,3,6,7]
-cuda_list = [0,1,2,3,4,5]
+cuda_list = [0,1,2,3,4,5,6]
 #cuda_list = [4,5,6,7]
 for ii in range(nn):
     job = 'UDA_{}'.format(ii)
