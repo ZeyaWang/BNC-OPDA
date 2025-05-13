996b41b951bed7a47f056e6e0d9e5c013c6dc4a7
ZWStatLab <zeya.wang@uky.edu>
2025-05-13 12:58:49 -0400

    update

--- make_exp.py
+++ make_exp.py
@@ -29,7 +29,7 @@ intervals = [1]
 #balances = [0.001, 0.01]
 lambdavs = [0.0]
 balances = [0.01]
-balances = [0.001, 0.1, 0, 1]
+#balances = [0.001, 0.1, 0, 1]
 lr_scales = [0.1]
 # lr_scales = [1, 0.1,0.01]
 # lr_scales = [0.001]
@@ -44,7 +44,7 @@ lrs = [0.001]#[0.01, 0.001, 0.0005, 0.0001]
 #lrs = [0.001, 0.0005]
 #KKs = [5, 10]#[5, 30, 100]
 #KKs = [50, 100]
-KKs = [50,100]
+KKs = [100]
 #KKs = [3,5]
 # covs = [0.001]#[0.01, 0.001]
 # covs = [0.0001]#[0.01, 0.001]
@@ -53,10 +53,12 @@ KKs = [50,100]
 covs = [0.001]
 #scs = ['cos', 'entropy']
 scs = ['cos']#, 'entropy']
-clf = [False, True] # [False, True]
+#clf = [False, True] # [False, True]
 clf = [True]
 #target_types = ['OPDA', 'OSDA']
 target_types = ['OPDA']
+nn = 4# 7
+
 for max_k in max_ks:
     for target_type in target_types:
         # if target_type == 'OSDA':
@@ -74,27 +76,27 @@ for max_k in max_ks:
                                         for cov in covs:
                                             for sc in scs:
                                                 for cl in clf:
+                                                    for _ in range(nn):
                                                     #CUDA_VISIBLE_DEVICES=1,3,5,7
-                                                    cmd = ('python /home/zwa281/UDA/BNC-OPDA/source_free.py --total_epoch 10 --target_type {} --dataset {} --source {} --target {} --balance {} --lr {} '
-                                                           '--lr_scale {} --iter_factor {} --lambdav {} --max_k {} --KK {} --covariance_prior {} --score {} ').format(target_type, ds, src, tar, balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc)
-                                                    if cl:
-                                                        cmd += '--classifier \n'
-                                                    else:
-                                                        cmd += '\n'
-                                                    outcsv = 'exp_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.csv'.format(domain[ds][src], domain[ds][tar], balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc, cl)
-                                                    if not os.path.isfile(outcsv):
-                                                        outline.append(cmd)
-                                                    else:
-                                                        print('======{} exists======'.format(outcsv))
-nn = 2# 7
+                                                        cmd = ('python /home/zwa281/UDA/BNC-OPDA/source_free_visda.py --total_epoch 10 --target_type {} --dataset {} --source {} --target {} --balance {} --lr {} '
+                                                               '--lr_scale {} --iter_factor {} --lambdav {} --max_k {} --KK {} --covariance_prior {} --score {} ').format(target_type, ds, src, tar, balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc)
+                                                        if cl:
+                                                            cmd += '--classifier \n'
+                                                        else:
+                                                            cmd += '\n'
+                                                    # outcsv = 'exp_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.csv'.format(domain[ds][src], domain[ds][tar], balance, lr, lr_scale, interval, lambdav, max_k, KK, cov, sc, cl)
+                                                    # if not os.path.isfile(outcsv):
+                                                    #     outline.append(cmd)
+                                                    # else:
+                                                    #     print('======{} exists======'.format(outcsv))
 split_lists = [[] for _ in range(nn)]
 for i, element in enumerate(outline):
     split_lists[i % nn].append(element)
 #cuda_list = [4,5]
-cuda_list = [6,7]
+#cuda_list = [6,7]
 cuda_list = [0,1,2,4, 5, 6]
 cuda_list = [0,1,2,3]#,4,5,6,7]
-cuda_list = [0,1]
+cuda_list = [2,3,4,5]
 #cuda_list = [2,4]
 for ii in range(nn):
     job = 'DAI_{}'.format(ii)
